32833608377b913de0182bfaed8eef6a5383151d
s2021 <s2021@e-mirim.hs.kr>
2022-04-18 13:40:49 +0900

    [ADD] Users 엔티티 생성

--- src/main/java/com/study/springstudy2/entity/Users.java
+++ src/main/java/com/study/springstudy2/entity/Users.java
@@ -1,4 +1,20 @@
 package com.study.springstudy2.entity;
 
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "users_tb")
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(length = 10, nullable = false, name = "user_name")
+    private String userName;
+
+    @Column(length = 30, nullable = false, name = "user_password")
+    private String userPassword;
 }
