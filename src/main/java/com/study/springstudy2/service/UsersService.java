3ebd6e9f1ffda89308e0f746aa89cbd1d8682c54
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-18 14:08:58 +0900

    [ADD] 유저 생성 기능 구현 (#5)
    * [ADD] H2 console 연결 세팅
    
    * [ADD] 유저 생성할 때 필요한 request dto 생성
    
    * [ADD] 유저 생성 기능 구현

--- src/main/java/com/study/springstudy2/service/UsersService.java
+++ src/main/java/com/study/springstudy2/service/UsersService.java
@@ -1,7 +1,24 @@
 package com.study.springstudy2.service;
 
+import com.study.springstudy2.dto.request.UserCreateRequestDto;
+import com.study.springstudy2.entity.Users;
+import com.study.springstudy2.entity.UsersRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@AllArgsConstructor
 public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    @Transactional
+    public Long createUser(UserCreateRequestDto dto){
+        Users users = Users.builder()
+                .userName(dto.getUserName())
+                .userPassword(dto.getUserPassword()).
+                build();
+        return usersRepository.save(users).getUserId();
+    }
 }
