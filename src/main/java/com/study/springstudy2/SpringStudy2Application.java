04527278d3b67d8cf3954e964911c3c98ce54c2a
s2021 <s2021@e-mirim.hs.kr>
2022-04-18 13:29:18 +0900

    Initial commit

---
+++ src/main/java/com/study/springstudy2/SpringStudy2Application.java
@@ -0,0 +1,13 @@
+package com.study.springstudy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudy2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/springstudy2/SpringStudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springstudy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
