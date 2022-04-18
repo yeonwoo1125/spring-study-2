3258174c3df347cdafe0b80b7a0c68546232a751
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-18 19:53:39 +0900

    [ADD] 회원 삭제 기능 구현

--- src/main/java/com/study/springstudy2/controller/UsersController.java
+++ src/main/java/com/study/springstudy2/controller/UsersController.java
@@ -1,22 +1,24 @@
 package com.study.springstudy2.controller;
 
 import com.study.springstudy2.dto.request.UserCreateRequestDto;
-import com.study.springstudy2.service.UsersService;
+import com.study.springstudy2.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
 public class UsersController {
-    private final UsersService usersService;
+    private final UserService usersService;
 
     @PostMapping
     public void createUser(@RequestBody UserCreateRequestDto dto){
         Long id = usersService.createUser(dto);
         System.out.println(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        usersService.deleteUser(id);
     }
 }
