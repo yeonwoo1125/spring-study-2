513239ed49f2f67044b9f8443353387116fe43c3
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-18 13:35:41 +0900

    [ADD] 프로젝트 파일 세팅 (#2)

---
+++ src/main/java/com/study/springstudy2/entity/UsersRepository.java
@@ -0,0 +1,6 @@
+package com.study.springstudy2.entity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsersRepository extends CrudRepository<Users, Long> {
+}
---
+++ src/main/java/com/study/springstudy2/service/UsersService.java
@@ -0,0 +1,7 @@
+package com.study.springstudy2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+}
