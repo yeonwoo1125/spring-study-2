ea6b267231cc9a0c1e59fe9f837d86528f4e5792
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-26 20:08:19 +0900

    [ADD] 기본생성자 접근 제어자 추가, 모든 멤버변수 생성자 추가

--- src/main/java/com/study/springstudy2/entity/User.java
+++ src/main/java/com/study/springstudy2/entity/User.java
@@ -1,15 +1,14 @@
 package com.study.springstudy2.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Table(name = "users_tb")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
